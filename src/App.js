1228247efe32d2bdb6630581da8404a85b5b999d
KevJSDevelopment <Kshhn12@gmail.com>
2020-12-21 13:09:13 -0500

    setup main app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
 
-function App() {
+const App = () => {
+
+  const fetchMovies = () => {
+    fetch("http://www.omdbapi.com/?apikey=[3d1fca0c]&s=batman")
+    .then((success) => { success.json() } )
+    .then((movies) => { console.log(movies) } )
+    .catch((error)=>{ console.log(error)});
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
