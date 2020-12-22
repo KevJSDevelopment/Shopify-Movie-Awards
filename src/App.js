ea49609b3604cf92e825c3c0764f3d03e79cea34
KevJSDevelopment <Kshhn12@gmail.com>
2020-12-21 21:08:24 -0500

    fetch request ready

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import React, { useEffect } from 'react'
 const App = () => {
 
   const fetchMovies = () => {
-    fetch("http://www.omdbapi.com/?apikey=[3d1fca0c]&s=batman")
-    .then((success) => { success.json() } )
-    .then((movies) => { console.log(movies) } )
-    .catch((error)=>{ console.log(error)});
+    // fetch("http://www.omdbapi.com/?apikey=[3d1fca0c]&s=batman")
+    // .then((success) => { success.json() } )
+    // .then((movies) => { console.log(movies) } )
+    // .catch((error)=>{ console.log(error)});
+    fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-byyear&page=1&year=2000", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "a2e3b55477msh0ea9f07ff156ef5p1c0080jsn43c96fb2724b",
+        "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com"
+      }  
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => {
+      console.error(err);
+    });
   }
 
   useEffect(() => {
